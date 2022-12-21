80c609e0d81a02cbcb11b3023ea1f699f3f32da4
Alex Alikhani <alikhanialex@gmail.com>
2022-12-21 10:24:52 +0000

    Adding created at text to make it clear what the date/time represents

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -9,7 +9,7 @@ const Ideas = () => {
         <div style={{ width: 300, border: "1px solid black", margin: 20 }}>
           <h2>{idea.title}</h2>
           <p>{idea.description}</p>
-          <p>{idea.createdAt}</p>
+          <p>Created at: {idea.createdAt}</p>
         </div>
       ))}
     </div>
