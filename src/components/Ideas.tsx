e3ef3509882f5104855ef3e350c7178d5ab38f17
Alex Alikhani <alikhanialex@gmail.com>
2022-12-21 19:47:56 +0000

    Sort ideas by creation date by default and added button to switch to sorting alphabetically and back to creation date

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Ideas = () => {
   const ideas = JSON.parse(localStorage.getItem("ideas") || "[]");
+  const [sortValue, setSortValue] = useState("created");
+
+  const sortIdeas = (sort: string) => {
+    if (sort === "alphabet") {
+      return ideas.sort((a: any, b: any) => a.title.localeCompare(b.title));
+    }
+    if (sort === "created") {
+      return ideas.sort((a: any, b: any) => a.createdAt.localeCompare(b.createdAt));
+    }
+  };
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-      {ideas.map((idea: any) => (
-        <div style={{ width: 300, border: "1px solid black", margin: 20 }}>
-          <h2>{idea.title}</h2>
-          <p>{idea.description}</p>
-          <p>Created at: {idea.createdAt}</p>
-        </div>
-      ))}
+    <div style={{ marginTop: 20 }}>
+      <button onClick={() => setSortValue(sortValue === "created" ? "alphabet" : "created")}>
+        {sortValue === "created" ? "Sort Ideas alphbetically" : "Sort ideas by creation date"}
+      </button>
+      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+        {sortIdeas(sortValue).map((idea: any) => (
+          <div style={{ width: 300, border: "1px solid black", margin: 20 }}>
+            <h2>{idea.title}</h2>
+            <p>{idea.description}</p>
+            <p>Created at: {idea.createdAt}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
