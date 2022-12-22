2283608db1af4c3ccb11b449cd18259cf02eafa2
Alex Alikhani <alikhanialex@gmail.com>
2022-12-22 20:42:25 +0000

    Test Idea tile renders with correct title text

---
+++ src/IdeaTile.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { fireEvent, getByTestId, render, screen } from "@testing-library/react";
+import App from "./App";
+import IdeaProvider from "./context/IdeaContext";
+import IdeaTile from "./components/IdeaTile";
+
+test("Idea Title is present in the document with its title text when there is an idea", async () => {
+  render(
+    <IdeaProvider>
+      <IdeaTile
+        idea={{
+          id: "123",
+          title: "test title",
+          description: "test description",
+          createdAt: new Date().toLocaleString(),
+        }}
+      />
+    </IdeaProvider>
+  );
+  const ideaTile = screen.getByTestId("idea-tile");
+  expect(ideaTile).toBeInTheDocument();
+  expect(screen.getByText("test title")).toBeInTheDocument();
+});
