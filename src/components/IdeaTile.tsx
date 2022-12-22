b9b285b1b6abb38c45d2ccf3fa5347a14b3fcd3b
Alex Alikhani <alikhanialex@gmail.com>
2022-12-22 16:59:56 +0000

    Functionality added to update description

--- src/components/IdeaTile.tsx
+++ src/components/IdeaTile.tsx
@@ -9,7 +9,9 @@ interface IdeaTileProps {
 const IdeaTile = ({ idea }: IdeaTileProps) => {
   const { ideas, setIdeas } = useContext(IdeaContext) as IdeaContextType;
   const [updatedTitle, setUpdatedTitle] = useState("");
+  const [updatedDescription, setUpdatedDescription] = useState("");
   const [editingTitle, setEditingTitle] = useState(false);
+  const [editingDescription, setEditingDescription] = useState(false);
 
   const handleDelete = (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -42,6 +44,30 @@ const IdeaTile = ({ idea }: IdeaTileProps) => {
     setEditingTitle(false);
   };
 
+  const handleUpdateDescription = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    //making a copy of the global state array of ideas
+    let ideasList = [...ideas];
+
+    // get index in ideas array of the selected idea
+    const index = ideasList.map((i) => i.description).indexOf(idea.description);
+
+    // making a copy of the selected idea
+    let selectedIdea = { ...ideasList[index] };
+
+    // updating the description of the selected idea
+    selectedIdea.description = updatedDescription;
+
+    // Adding the selected idea with updated title back to its place in the array
+    ideasList[index] = selectedIdea;
+
+    // Set the global ideas array to the new mutated array with the updated target idea
+    setIdeas(ideasList);
+    localStorage.setItem("ideas", JSON.stringify(ideasList));
+
+    setEditingTitle(false);
+  };
+
   return (
     <div>
       <div style={{ width: 300, border: "1px solid black", margin: 20 }}>
@@ -58,7 +84,21 @@ const IdeaTile = ({ idea }: IdeaTileProps) => {
             <button onClick={(e) => handleUpdateTitle(e)}>Update title</button>
           </div>
         </div>
-        <p>{idea.description}</p>
+
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <p>{idea.description}</p>
+          <button
+            style={{ display: !editingDescription ? "block" : "none", width: 100 }}
+            onClick={() => setEditingDescription(true)}
+          >
+            Edit Description
+          </button>
+          <div style={{ display: editingDescription ? "flex" : "none" }}>
+            <input type="text" onChange={(e) => setUpdatedDescription(e.target.value)} />
+            <button onClick={(e) => handleUpdateDescription(e)}>Update Description</button>
+          </div>
+        </div>
+
         <p>Created at: {idea.createdAt}</p>
         <button onClick={(e) => handleDelete(e)}>Delete</button>
       </div>
