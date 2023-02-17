d24087010b09c6d0c1c560e6234f8f5e6c75235e
Alex <alikhanialex@gmail.com>
2023-02-17 21:04:39 +0000

    Moving Idea Form related tests into IdeaForm test file from Ideas test file

--- src/tests/IdeaForm.test.tsx
+++ src/tests/IdeaForm.test.tsx
@@ -67,3 +67,37 @@ test("Delete idea is called", async () => {
 
 });
 
+test("Idea displays with a default title value from the idea in context and will update when changed", async () => {
+  const mockIdea = {  id: '123',  title: 'test title',  description: 'test description',  createdAt: '14/01/2023, 20:19:34'}
+
+  render(<IdeaForm idea={mockIdea}/>)
+
+  expect(screen.getByRole("textbox", { name: /title/i })).toHaveValue(
+    "test title"
+  );
+  
+  await user.type(screen.getByRole("textbox", { name: /title/i }), '123');
+
+  expect(screen.getByRole("textbox", { name: /title/i })).toHaveValue(
+    "test title123"
+  );
+}
+)
+
+test("Idea displays with an empty title when user removes the text", async () => {
+  const mockIdea = {  id: '123',  title: 'test title',  description: 'test description',  createdAt: '14/01/2023, 20:19:34'}
+
+  render(<IdeaForm idea={mockIdea}/>)
+
+  expect(screen.getByRole("textbox", { name: /title/i })).toHaveValue(
+    "test title"
+  );
+  
+  await user.clear(screen.getByRole("textbox", { name: /title/i }));
+
+  expect(screen.getByRole("textbox", { name: /title/i })).toHaveValue(
+    ""
+  );
+}
+)
+
--- src/tests/Ideas.test.tsx
+++ src/tests/Ideas.test.tsx
@@ -2,27 +2,6 @@
 import Ideas from "../components/Ideas";
 import { render } from "./utils/test-utils";
 import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-
-
-const user = userEvent.setup()
-
-test("Idea displays with a default title value from the idea in context and will update when changed", async () => {
-  const mockIdeas = [{  id: '123',  title: 'test title',  description: 'test description',  createdAt: '14/01/2023, 20:19:34'}];
-
-  render(<Ideas />, {ideas: mockIdeas})
-
-  expect(screen.getByRole("textbox", { name: /title/i })).toHaveValue(
-    "test title"
-  );
-  
-  await user.type(screen.getByRole("textbox", { name: /title/i }), '123');
-
-  expect(screen.getByRole("textbox", { name: /title/i })).toHaveValue(
-    "test title123"
-  );
-}
-)
 
 
 test("2 ideas from the context render with their correct default titles", () => {
