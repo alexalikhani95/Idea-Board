791c0507a42baedc60f7d11ad437321817cf3790
Alex <alikhanialex@gmail.com>
2023-03-17 22:36:33 +0000

    Refactoring/ cleaning up component files

--- src/components/IdeaForm.tsx
+++ src/components/IdeaForm.tsx
@@ -1,72 +1,72 @@
-import "../styles/IdeaForm.css";
-import { useForm} from "react-hook-form";
-import {v4 as uuidv4} from 'uuid'
-import { IdeaContextType, IdeaType} from "../types/Idea";
-import { useContext, useEffect, useState } from "react";
-import {IdeaContext} from "../context/IdeaContext";
+import '../styles/IdeaForm.css';
+import { useForm } from 'react-hook-form';
+import { v4 as uuidv4 } from 'uuid';
+import { IdeaContextType, IdeaType } from '../types/Idea';
+import { useContext, useEffect, useState } from 'react';
+import { IdeaContext } from '../context/IdeaContext';
 
-type FormType = {
-  idea?: IdeaType
-}
-
-type Inputs = {
-  title: string,
-  description: string,
+type Props = {
+  idea?: IdeaType;
 };
 
-const IdeaForm = ({idea}: FormType) => {
+type Inputs = {
+  title: string;
+  description: string;
+};
+
+const IdeaForm = ({ idea }: Props) => {
   const [showUpdatedText, setShowUpdatedText] = useState(false);
   //@ts-ignore
-  const {handleAddIdea, handleUpdateIdea, handleDeleteIdea } = useContext(IdeaContext) as IdeaContextType;
-  const { register, handleSubmit, formState: { errors }, reset, watch } = useForm<Inputs>({
+  const { handleAddIdea, handleUpdateIdea, handleDeleteIdea } = useContext(
+    IdeaContext
+  ) as IdeaContextType;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    watch,
+  } = useForm<Inputs>({
     defaultValues: {
-      title: idea ? idea.title : '',
-      description: idea?  idea.description: ''
-    }
+      title: idea?.title || '',
+      description: idea?.description || '',
+    },
   });
 
-  const handleDelete = () => {
-    if(!idea) {
-      return
-    }
-    handleDeleteIdea(idea.id)
-  };
-
-
-  const submitForm = ( data: Inputs) => {
+  const submitForm = ({ title, description }: Inputs) => {
     handleAddIdea({
       id: uuidv4(),
-      title: data.title,
-      description: data.description,
+      title,
+      description,
       createdAt: new Date().toLocaleString(),
-    })
+    });
 
-    reset()
-  }
+    reset();
+  };
 
   const updateTitle = (newTitle: string) => {
-    if(!idea || newTitle === idea.title) {
-      return
+    if (!idea || newTitle === idea.title) {
+      return;
     }
     handleUpdateIdea({
       ...idea,
       title: newTitle,
-     updatedAt: new Date().toLocaleString(),
-    })
-    setShowUpdatedText(true)
-  }
+      updatedAt: new Date().toLocaleString(),
+    });
+    setShowUpdatedText(true);
+  };
 
   const updateDescription = (newDescription: string) => {
-    if(!idea || newDescription === idea.description) {
-      return
+    if (!idea || newDescription === idea.description) {
+      return;
     }
     handleUpdateIdea({
       ...idea,
       description: newDescription,
-     updatedAt: new Date().toLocaleString(),
-    })
-    setShowUpdatedText(true)
-  }
+      updatedAt: new Date().toLocaleString(),
+    });
+    setShowUpdatedText(true);
+  };
 
   useEffect(() => {
     setTimeout(function () {
@@ -76,49 +76,61 @@ const IdeaForm = ({idea}: FormType) => {
 
   return (
     <div className="idea-form-container">
-     {!idea && <h2>Add an idea</h2> }
-      
+      {!idea && <h2>Add an idea</h2>}
+
       <form onSubmit={handleSubmit(submitForm)}>
         <div>
           <label>
             Title
-          <input
-            type="text"
-            autoFocus={!idea}
-            {...register("title", { required: true })}
-            onBlur={(e) => updateTitle(e.target.value)}
-            className={idea ? 'update-input' : ''}
-          />
+            <input
+              type="text"
+              autoFocus={!idea}
+              {...register('title', { required: true })}
+              onBlur={(e) => updateTitle(e.target.value)}
+              className={idea ? 'update-input' : ''}
+            />
           </label>
         </div>
-        {errors.title?.type === 'required' && <span className="error-text">A title is required</span>}
+        {errors.title?.type === 'required' && (
+          <span className="error-text">A title is required</span>
+        )}
         <div className="description-container">
           <label>
             Description
-          <input
-            {...register("description", { required: true })}
-            maxLength={140}
-            onBlur={(e) => updateDescription(e.target.value)}
-            className={idea ? 'update-input' : ''}
-          />
+            <input
+              {...register('description', { required: true })}
+              maxLength={140}
+              onBlur={(e) => updateDescription(e.target.value)}
+              className={idea ? 'update-input' : ''}
+            />
           </label>
         </div>
         {showUpdatedText && <h3 className="updated-text">Idea updated!</h3>}
-        {errors.description?.type === 'required' && <span className="error-text">A description is required</span>}
-        {!idea && 
-        <>
-        <p>Description Characters remaining: {140 - watch("description").length} / 140</p>
-        <button type="submit" className="add-idea-button">
-          Submit
-        </button>
-        </>
-        }
+        {errors.description?.type === 'required' && (
+          <span className="error-text">A description is required</span>
+        )}
+        {!idea && (
+          <>
+            <p>
+              Description Characters remaining:{' '}
+              {140 - watch('description').length} / 140
+            </p>
+            <button type="submit" className="add-idea-button">
+              Submit
+            </button>
+          </>
+        )}
       </form>
-      {idea && <p>Created at: {idea.createdAt}</p> }
-        {idea && idea.updatedAt && <p>Updated at: {idea.updatedAt}</p>}
-      {idea && <button className="delete-idea-button" onClick={handleDelete}>
+      {idea && <p>Created at: {idea.createdAt}</p>}
+      {idea && idea.updatedAt && <p>Updated at: {idea.updatedAt}</p>}
+      {idea && (
+        <button
+          className="delete-idea-button"
+          onClick={() => handleDeleteIdea(idea.id)}
+        >
           Delete
-        </button> }
+        </button>
+      )}
     </div>
   );
 };
