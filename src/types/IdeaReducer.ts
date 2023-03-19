518128e255d735306fdc073f09c0ebce98548d22
Alex <alikhanialex@gmail.com>
2023-03-19 13:36:37 +0000

    Moving type out of its own file to ideaRuducer file as its only used there

--- src/types/IdeaReducer.ts
+++
@@ -1,8 +0,0 @@
-import { IdeaType } from './Idea';
-
-export type Action =
-  | { type: 'added'; idea: IdeaType }
-  | { type: 'delete'; id: string }
-  | { type: 'update'; idea: IdeaType }
-  | { type: 'sort_alphabetical' }
-  | { type: 'sort_created' };
